972508edf066370b894e0276f4cfaa2a594f3fd1
resistan <resistan@gmail.com>
2025-07-17 12:47:00 +0900

    feat: Add KWCAG 2.2 documentation and guidelines
    - Introduced new sections for web application accessibility compliance (4.2.1).
    - Created an overview of robustness in web content (4).
    - Added understanding of KWCAG and its relationship with WCAG (5).
    - Provided practical application guidelines for KWCAG (5).
    - Updated the document introduction and revision history.
    - Included detailed checklists and examples for accessibility compliance.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,12 +11,12 @@ const commonKeywords = "웹, 접근성, 콘텐츠, 제작기법, KWCAG2.2, acces
 
 export default defineConfig({
     site: 'https://nia-a11y.github.io/',
-    base: process.env.NODE_ENV === '/',
+    base: process.env.NODE_ENV = '/',
 	integrations: [
         astroExpressiveCode(),
         mdx(),
 		starlight({
-            title: '웹 접근성을 고려한 콘텐츠 제작기법 2.2',
+            title: 'NIA 디지털 접근성',
             defaultLocale: 'root',
             locales: {
                 root: {
@@ -40,128 +40,134 @@ export default defineConfig({
                 './src/styles/custom.css'
             ],
 			sidebar: [
-				{
-					label: '인식의 용이성',
-                    collapsed: true,
+                {
+                    label: '웹 접근성을 고려한 콘텐츠 제작기법 2.2',
+                    collapsed: false,
                     items: [
                         {
-                            label: '개요',
-                            link: '1'
+                            label: '문서 소개',
+                            link: 'kwcag22tech/',
                         },
                         {
-                            label: '1.1 대체 텍스트',
+                            label: '인식의 용이성',
                             collapsed: true,
-                            autogenerate: { directory: '1/1' },
+                            items: [
+                                {
+                                    label: '개요',
+                                    link: 'kwcag22tech/1'
+                                },
+                                {
+                                    label: '1.1 대체 텍스트',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/1/1' },
+                                },
+                                {
+                                    label: '1.2 멀티미디어 대체수단',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/1/2' },
+                                },
+                                {
+                                    label: '1.3 적응성',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/1/3' },
+                                },
+                                {
+                                    label: '1.4 명료성',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/1/4' },
+                                },
+                            ]
                         },
                         {
-                            label: '1.2 멀티미디어 대체수단',
+                            label: '운용의 용이성',
                             collapsed: true,
-                            autogenerate: { directory: '1/2' },
+                            items: [
+                                {
+                                    label: '개요',
+                                    link: 'kwcag22tech/2'
+                                },
+                                {
+                                    label: '2.1 입력장치 접근성',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/2/1' },
+                                },
+                                {
+                                    label: '2.2 충분한 시간 제공',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/2/2' },
+                                },
+                                {
+                                    label: '2.3 광과민성 발작 예방',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/2/3' },
+                                },
+                                {
+                                    label: '2.4 쉬운 내비게이션',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/2/4' },
+                                },
+                                {
+                                    label: '2.5 입력 방식',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/2/5' },
+                                },
+                            ]
                         },
                         {
-                            label: '1.3 적응성',
+                            label: '이해의 용이성',
                             collapsed: true,
-                            autogenerate: { directory: '1/3' },
+                            items: [
+                                {
+                                    label: '개요',
+                                    link: 'kwcag22tech/3'
+                                },
+                                {
+                                    label: '3.1 가독성',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/3/1' },
+                                },
+                                {
+                                    label: '3.2 예측 가능성',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/3/2' },
+                                },
+                                {
+                                    label: '3.3 입력 도움',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/3/3' },
+                                },
+                            ]
                         },
                         {
-                            label: '1.4 명료성',
+                            label: '견고성',
                             collapsed: true,
-                            autogenerate: { directory: '1/4' },
+                            items: [
+                                {
+                                    label: '개요',
+                                    link: 'kwcag22tech/4'
+                                },
+                                {
+                                    label: '4.1 문법 준수',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/4/1' },
+                                },
+                                {
+                                    label: '4.2 웹 애플리케이션 접근성',
+                                    collapsed: true,
+                                    autogenerate: { directory: 'kwcag22tech/4/2' },
+                                },
+                            ]
+                        },
+                        {
+                            label: 'KWCAG 이해와 실무 적용',
+                            collapsed: true,
+                            autogenerate: { directory: 'kwcag22tech/5' },
+                        },
+                        {
+                            'label': '문서 갱신 이력',
+                            link: 'kwcag22tech/update',
                         },
                     ]
-				},
-				{
-                    label: '운용의 용이성',
-                    collapsed: true,
-                    items: [
-                        {
-                            label: '개요',
-                            link: '2'
-                        },
-                        {
-                            label: '2.1 입력장치 접근성',
-                            collapsed: true,
-                            autogenerate: { directory: '2/1' },
-                        },
-                        {
-                            label: '2.2 충분한 시간 제공',
-                            collapsed: true,
-                            autogenerate: { directory: '2/2' },
-                        },
-                        {
-                            label: '2.3 광과민성 발작 예방',
-                            collapsed: true,
-                            autogenerate: { directory: '2/3' },
-                        },
-                        {
-                            label: '2.4 쉬운 내비게이션',
-                            collapsed: true,
-                            autogenerate: { directory: '2/4' },
-                        },
-                        {
-                            label: '2.5 입력 방식',
-                            collapsed: true,
-                            autogenerate: { directory: '2/5' },
-                        },
-                    ]
-				},
-                {
-                    label: '이해의 용이성',
-                    collapsed: true,
-                    items: [
-                        {
-                            label: '개요',
-                            link: '3'
-                        },
-                        {
-                            label: '3.1 가독성',
-                            collapsed: true,
-                            autogenerate: { directory: '3/1' },
-                        },
-                        {
-                            label: '3.2 예측 가능성',
-                            collapsed: true,
-                            autogenerate: { directory: '3/2' },
-                        },
-                        {
-                            label: '3.3 입력 도움',
-                            collapsed: true,
-                            autogenerate: { directory: '3/3' },
-                        },
-                    ]
-                },
-                {
-                    label: '견고성',
-                    collapsed: true,
-                    items: [
-                        {
-                            label: '개요',
-                            link: '4'
-                        },
-                        {
-                            label: '4.1 문법 준수',
-                            collapsed: true,
-                            autogenerate: { directory: '4/1' },
-                        },
-                        {
-                            label: '4.2 웹 애플리케이션 접근성',
-                            collapsed: true,
-                            autogenerate: { directory: '4/2' },
-                        },
-                    ]
-                },
-                {
-                    label: 'KWCAG 이해와 실무 적용',
-                    collapsed: true,
-                    autogenerate: { directory: '5' },
-                },
-                {
-                    'label': '문서 소개',
-                    link: '/intro',
-                },
-                {
-                    'label': '문서 갱신 이력',
-                    link: '/update',
                 },
 			],
 		}),
